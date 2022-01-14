b977f549cc0b3a6fa92ddd1c8629e48881b3fbc0
Fred Awumey <fredawumeyfafa@gmail.com>
2022-01-14 21:23:00 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+<script src="js/jquery.min.js" type="text/javascript"></script>
+<script src="js/jquery.magnific-popup.min.js" type="text/javascript"></script>
+<script src="js/isotope/isotope.min.js"></script>
+
+$(document).ready(function () {
+
+
+    let $btns = $('.project-area .button-group button');
+
+
+    $btns.click(function (e) {
+
+        $('.project-area .button-group button').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('.project-area .grid').isotope({
+            filter: selector
+        });
+
+        return false;
+    })
+
+    $('.project-area .button-group #btn1').trigger('click');
+
+    $('.project-area .grid .test-popup-link').magnificPopup({
+        type: 'image',
+        gallery: { enabled: true }
+    });
+
+
+});
